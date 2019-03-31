9f98b4a56d90dc9bed0b9fc5f176c762996ae0cd
mrtan <1003306141@qq.com>
2019-03-31 22:54:10 +0800

    CrackMe004 Completed

---
+++ 004/算法分析/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char name[20];
+	printf("please input your name.\n");
+	scanf("%s", name);
+	int code = 5 + strlen(name);
+	printf("ºÚÍ·Sun Bird%ddseloffc-012-OK%s\n",code,name);
+	return 0;
+}
